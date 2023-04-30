bb4b369f5218105e724571bfbb05fb326220ea83
newlog <newlog@overflowedminds.net>
2023-04-30 20:44:39 +0200

    Use memory by using ConversationalRetrievalChain

--- documentation_helper/backend/core.py
+++ documentation_helper/backend/core.py
@@ -1,8 +1,9 @@
 import os
+from typing import Any
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 
 import pinecone
@@ -14,7 +15,7 @@ pinecone.init(
 )
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: list[tuple[str, Any]] = None):
     embeddings = OpenAIEmbeddings()
     vector_store = Pinecone.from_existing_index(
         index_name=PINECONE_INDEX_NAME, embedding=embeddings
@@ -22,13 +23,13 @@ def run_llm(query: str):
     chat = ChatOpenAI(verbose=True, temperature=0)
     # stuff chain type only means to get the context and insert (stuff) it as the prompt context
     # the retriever is just a wrapper around the vector_store that allows us to retrieve similar vectors
-    qa = RetrievalQA.from_chain_type(
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
         chain_type="stuff",
         retriever=vector_store.as_retriever(),
         return_source_documents=True,
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
